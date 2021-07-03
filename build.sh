24386a4fd1936c9732e5c1f9e6c92b3fd192f473
Von Grossi <vongrossi@gmai.com>
2021-07-03 15:54:14 -0300

    changed user repo and testing version

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-docker build -t christoofar/cockroachdb-arm64:20.2.11 -t christoofar/cockroachdb-arm64:latest .
+docker build -t vongrossi/cockroachdb-arm64:21.1.3 -t vongrossi/cockroachdb-arm64:latest .
+
